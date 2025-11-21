597084c5eb0a616708e6592f936568a54e75c56e
kkkyaw2k <120306055+kkkyaw2k@users.noreply.github.com>
2025-11-22 00:03:17 +0800

    fix alert to stop negative anomaly counter

--- alert_engine.py
+++ alert_engine.py
@@ -11,7 +11,7 @@ def alert_engine(logs, eventsList, baseline, anomoly_threshold):
             log_value = log[event]
 
             if baseline_sd != 0:
-                num_standard_deviations = (log_value - baseline_mean) / baseline_sd
+                num_standard_deviations = abs((log_value - baseline_mean)) / baseline_sd # made it abs value to get rid of negative anomalies
                 weighted_deviation = num_standard_deviations * details['weight']
                 total_anomaly_counter += weighted_deviation
         
